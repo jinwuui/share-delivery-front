4fc86f3055c9e38af4b656bbab105255632ecc48
jinoo7099 <vxc7099@gmail.com>
2022-04-30 16:10:05 +0900

    docs fcm project setting

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,15 @@ import 'package:get/get.dart';
 import 'package:share_delivery/src/controller/root_controller.dart';
 import 'package:share_delivery/src/root.dart';
 import 'package:share_delivery/src/routes/route.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
