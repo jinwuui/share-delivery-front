edd60cd3edb02f46ffc4a8770f93b8192c4da75e
jinoo7099 <vxc7099@gmail.com>
2022-05-05 18:42:27 +0900

    add delivery order tab controller 추가
    - 동적으로 탭 이동 가능

---
+++ lib/src/controller/delivery_order_detail/delivery_order_tab_controller.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class DeliveryOrderTabController extends GetxController
+    with GetTickerProviderStateMixin {
+  List<Tab> tabChildParameters = [
+    Tab(
+      child: Container(
+        alignment: Alignment.center,
+        constraints: BoxConstraints.expand(),
+        child: Text("상세 정보"),
+      ),
+    ),
+    Tab(
+      child: Container(
+        alignment: Alignment.center,
+        constraints: BoxConstraints.expand(),
+        child: Text("주문 정보"),
+      ),
+    ),
+    Tab(
+      child: Container(
+        alignment: Alignment.center,
+        constraints: BoxConstraints.expand(),
+        child: Text("채팅방"),
+      ),
+    ),
+  ];
+  static DeliveryOrderTabController get to => Get.find();
+  DeliveryOrderTabController();
+
+  late TabController controller;
+
+  @override
+  void onInit() {
+    print("Delivery Order Controller INit");
+
+    super.onInit();
+    controller = TabController(vsync: this, length: tabChildParameters.length);
+    asyncLoadTabs();
+  }
+
+  void asyncLoadTabs({int index = 0}) async {
+    controller.dispose(); // release animation resources
+    controller = TabController(
+        vsync: this,
+        length: tabChildParameters.length,
+        initialIndex: index // to show a particular tab on create
+        );
+    update();
+  }
+
+  void switchTab(int index) async {
+    asyncLoadTabs(index: index);
+  }
+
+  @override
+  void onClose() {
+    controller.dispose();
+    super.onClose();
+  }
+}
